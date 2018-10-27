f786a7c892f067caefc0409671b07815f42f7236
Karim shoair <d4vinci@protonmail.com>
2018-10-27 02:04:12 +0200

    Uncommented version checker :3

--- PasteJacker/Core/updater.py
+++ PasteJacker/Core/updater.py
@@ -7,15 +7,13 @@ from urllib.request import urlopen
 def check():
 	f = open( utils.add_corefilepath("Data","version.txt"), 'r')
 	file_data = f.read().strip()
-	version = file_data
+	try:
+		version = urlopen('https://raw.githubusercontent.com/D4Vinci/PasteJacker/master/PasteJacker/Core/Data/version.txt').read().decode('utf-8').strip()
+	except:
+		error("Can't reach Internet !!!")
+		sys.exit(0)
+
 	if version != file_data:
 		return file_data+R+" but new version is available!"
 	else:
 		return file_data
-"""
-	try:
-		version = urlopen('https://raw.githubusercontent.com/D4Vinci/PasteJacker/master/Core/Data/version.txt').read().decode('utf-8').strip()
-	except:
-		error("Can't reach Internet !!!")
-		sys.exit(0)
-"""
